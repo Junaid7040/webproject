af2bd62f90fa47597d707450a99d1973d33781f1
Junaid7040@gmail.com <Junaid7040@gmail.com>
2023-03-08 02:40:12 +0500

    server.js

---
+++ contollers/home.js
@@ -0,0 +1,32 @@
+exports.home =function (req,res){
+
+    res.send("home page");   
+};
+
+exports.plot =function (req,res){
+
+    res.send("plot page");   
+};
+
+exports.house =function (req,res){
+
+    res.send("house page");   
+};
+
+exports.rent =function (req,res){
+
+    res.send("rent page");   
+};
+exports.about =function (req,res){
+
+    res.send("about page");   
+};
+exports.front=function(req,res)
+{
+    res.send("Front Page");
+}
+
+exports.contactus =function (req,res){
+
+    res.send("contact page");   
+};
